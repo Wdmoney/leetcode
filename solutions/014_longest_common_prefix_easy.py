c073e977ae234376c8b1c7a3541c9a3c78d52dc2
Wdmoney <jw@daytrade.ru>
2023-08-15 09:14:37 +0300

    longestCommonPrefix1

--- solutions/014_longest_common_prefix_easy.py
+++ solutions/014_longest_common_prefix_easy.py
@@ -20,6 +20,18 @@ class Solution:
                 prefresult = pref
                 pref = word[0:i+1]
         return prefresult
+# the best solution (the most important thing is the sorted(v))
+
+    def longestCommonPrefix1(self, v: list) -> str:
+        ans = ""
+        v = sorted(v)
+        first = v[0]
+        last = v[-1]
+        for i in range(min(len(first), len(last))):
+            if(first[i] != last[i]):
+                return ans
+            ans += first[i]
+        return ans
 
 
 strs = ["flower", "flow", "flight"]
